4d5bca2b635543f59593cf89acd4f99085f995b9
I Gede Bagus Wirawan <gusdechang24@gmail.com>
2021-03-24 18:22:39 +0800

    Landing Page Beasiswa NTB Dalam Negeri

---
+++ index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>Beasiswa NTB Dalam Negeri</title>
+	<link rel="icon" href="assets/ntb.png" type="image/png" sizes="16x16">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700,800,900%7COpen+Sans:300,400,500" rel="stylesheet">
+	<link rel="stylesheet" href="assets/css/animate.css"> <!-- Resource style -->
+	<link rel="stylesheet" href="assets/css/owl.carousel.css">
+	<link rel="stylesheet" href="assets/css/owl.theme.css">
+	<link rel="stylesheet" href="assets/css/ionicons.min.css"> <!-- Resource style -->
+	<link href="assets/css/stylesheet.css" rel="stylesheet" type="text/css" media="all" />
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
+</head>
+
+<body class="boxed-layout">
+	<div class="wrapper">
+		<!-- Navbar Section -->
+		<nav class="navbar navbar-expand-md navbar-light bg-light fixed-top wt-border">
+			<div class="container">
+				<a class="navbar-brand" href="#"> <i class="fa fa-graduation-cap" aria-hidden="true"></i> <b>Beasiswa NTB</b> <br> <span style="font-size:15px">Dalam Negeri</span> </a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav ml-auto navbar-right">
+						<li class="nav-item"><a href="#main" style="font-size:25px; color:#999"> <i class="fab fa-facebook-square"></i> </a></li>
+						<li class="nav-item"><a href="https://www.instagram.com/beasiswantb_dalamnegeri/" style="font-size:25px; color:#999"> <i class=" fab fa-instagram "></i> </a></li>
+					</ul>
+				</div>
+		</nav><!-- Navbar End -->
+
+
+		<div id=" main" class="main">
+			<div class="home">
+				<div class="container">
+					<div class="hero-content wow fadeIn">
+						<div class="flex-split">
+							<div class="container">
+								<div class="flex-inner flex-inverted align-center">
+									<div class="f-image f-image-inverted">
+										<img class="img-fluid" src="assets/images/landing.png" alt="Feature">
+									</div>
+									<div class="f-text">
+										<div class="left-content">
+											<h4 style="line-height: 25px;">Biro Kesejahteraan Rakyat Sekretariat Daerah <br> Provinsi Nusa Tenggara Barat</h4>
+											<h2>Beasiswa NTB Dalam Negeri</h2>
+											<p>Beasiswa NTB Dalam Negeri merupakan Beasiswa yang diberikan kepada masyarakat/mahasiswa miskin asal NTB yang mengikuti pendidikan tinggi di Perguruan Tinggi di dalam negeri</p>
+											<a class="btn-action btn-outline nav-link js-scroll-trigger" href="https://www.instagram.com/beasiswantb_dalamnegeri/"><i class=" fab fa-instagram "></i> Instagram</a>
+											<a class="btn-action" href="#"><i class="fab fa-facebook-square"></i> Facebook</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div><!-- Hero End -->
+
+
+			<!--- About Section -->
+			<div class="yd-cat">
+				<div class="container">
+					<div class="cat-inner">
+						<div class="cat-flex">
+							<div class="cat2">
+								<h4>Jenis-jenis</h4>
+								<h2>Beasiswa NTB Terbagi menjadi 3 Jenis :</h2>
+								<p> Pilih salah satu untuk informasi lebih lanjut.</p>
+							</div>
+						</div>
+						<div class="cat-flex">
+							<style>
+								.custom-hover:hover {
+									background-color: #fff2e5;
+								}
+							</style>
+							<div class="cat1 ">
+								<a href="https://bmb.beasiswantbdalamnegeri.org">
+									<div class="cat-item clr2 custom-hover">
+										<div class="cat-icon">
+											<div class="cat-img">
+												<i class="fas fa-user-graduate" style="font-size: 50px; color:#666"></i>
+											</div>
+										</div>
+										<div class="cat-text">
+											<h3>Beasiswa Miskin Berprestasi</h3>
+										</div>
+									</div>
+								</a>
+							</div>
+							<div class="cat1">
+								<a href="https://bsu.beasiswantbdalamnegeri.org">
+									<div class="cat-item clr2 custom-hover">
+										<div class="cat-icon">
+											<div class="cat-img">
+												<i class="fa fa-graduation-cap" style="font-size: 50px; color:#666" aria-hidden="true"></i>
+											</div>
+										</div>
+										<div class="cat-text">
+											<h3>Beasiswa Stimulan Unggulan</h3>
+										</div>
+									</div>
+								</a>
+							</div>
+							<div class="cat1">
+								<a href="https://bsk.beasiswantbdalamnegeri.org">
+									<div class="cat-item clr2 custom-hover">
+										<div class="cat-icon">
+											<div class="cat-img">
+												<i class="fas fa-handshake" style="font-size: 50px; color:#666"></i>
+											</div>
+										</div>
+										<div class="cat-text">
+											<h3>Beasiswa Stimulan Kerjasama</h3>
+										</div>
+									</div>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+			<!-- Footer -->
+			<div class="footer">
+				<div class="container">
+					<div class="row text-center align-items-center">
+						<div class="col-lg-4 col-md-3 col-sm-12">
+							<div class="footer-logo">
+								<h2><i class="fa fa-graduation-cap" aria-hidden="true"></i> <b>Beasiswa NTB</b> <br> <span style="font-size:15px">Dalam Negeri</span></h2>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6 col-sm-12">
+							<ul class="footer-menu">
+								<li><a href="https://bmb.beasiswantbdalamnegeri.org">B M B</a> </li>
+								<li><a href="https://bsu.beasiswantbdalamnegeri.org">B S U</a> </li>
+								<li><a href="https://bsk.beasiswantbdalamnegeri.org">B S K</a> </li>
+							</ul>
+						</div>
+						<div class="col-lg-4 col-md-3 col-sm-12">
+							<div class="footer-links">
+								<ul>
+									<li><a href="#main" style="font-size:25px; color:#999"> <i class="fab fa-facebook-square"></i> </a> </li>
+									<li><a href="#main" style="font-size:25px; color:#999"> <i class=" fab fa-instagram "></i> </a> </li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- Scroll To Top -->
+					<a id="back-top" class="back-to-top js-scroll-trigger" href="#main"></a>
+					<!-- Scroll To Top Ends-->
+				</div>
+			</div>
+		</div> <!-- Main -->
+	</div><!-- Wrapper -->
+
+
+	<!-- Jquery and Js Plugins -->
+	<script src="assets/js/jquery-2.1.1.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/jquery.validate.min.js"></script>
+	<script src="assets/js/plugins.js"></script>
+	<script src="assets/js/custom.js"></script>
+</body>
+
+</html>
